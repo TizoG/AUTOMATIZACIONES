d3bdaaeda50aae2284162e848cc67039f4ce4a5f
TizoG <tirado114@hotmail.com>
2025-03-06 11:30:27 +0100

    documentacion de codigo y formateo la salida por terminal

--- 00 Proyectos/Extraccion de numeros de telefono y correos electronicos/Tizog.py
+++ 00 Proyectos/Extraccion de numeros de telefono y correos electronicos/Tizog.py
@@ -4,22 +4,38 @@ import pyperclip
 # Recuperamos el texto copiado en la variable texto
 texto = pyperclip.paste()
 
+# asignamos a la variable la expresion regular que nos buscara los números de telefonos
 numeros_telefono = re.compile(
     r"""
-    \+?\d{1,4}[\s-]?              # Código de país, opcional (+34, 001, etc.)
-    \(?\d{1,4}\)?[\s-]?           # Código de área (puede tener paréntesis)
-    \d{3}                         # 3 dígitos
-    [\s-]?                         # Separador opcional (espacio o guion)
-    \d{3}                         # 3 dígitos
-    [\s-]?                         # Separador opcional (espacio o guion)
-    \d{3,4}                       # Últimos 3 o 4 dígitos
+    \+?\d{1,4}[\s-]?                # Código de país, opcional (+34, 001, etc.)
+    \(?\d{1,4}\)?[\s-]?             # Código de área (puede tener paréntesis, 1 a 4 dígitos)
+    \d{1,3}                         # Primer grupo de 1 a 3 dígitos
+    [\s-]?                           # Separador opcional (espacio o guion)
+    \d{3}                            # Segundo grupo de 3 dígitos
+    [\s-]?                           # Separador opcional (espacio o guion)
+    \d{3,4}                          # Últimos 3 o 4 dígitos
     """, re.VERBOSE)  # Modo verbose para facilitar la lectura
+
+# en esta variable asignamos el findall para que busque todos los números de telefono.
 telefonos = numeros_telefono.findall(
     texto)
 
-print(telefonos)
+# formateamos para verlo mejor
+print("*"*20)
+print()
+print("Los números de telefonos encontrados son: ")
+print()
+
+# Iniciamos un bucle for para que nos lo de cada uno en una linea diferente y no en una lista
+for numero in telefonos:
+    print(numero)
 
 
+print()
+print("*"*20)
+
+
+# creamos la variable que le asignamos la expresión regular para que ahora nos encuentre emails. Aquí solo queremos los emails con extensiones (hotmail o gmail) y las(.com,.es,.net)
 emails = re.compile(
     r"""
     [a-zA-Z0-9]+            # Letras o números al inicio
@@ -29,9 +45,23 @@ emails = re.compile(
     (?:\.com|\.es|\.net)      # Extensiones permitidas
     """, re.VERBOSE
 )
-email = emails.findall(
-    "aqui puede haber un email pepe@hotmail.com tambien puede haber  juan.perez@gmail.es o ya si eso  user_123-hotmail.net")
-print(email)
 
-# TODO: Buscar coincidencias en el texto del portapapeles
-# TODO: Copiar resultados al portapapeles y mostrar mensaje
+# variable para buscar todos los correos
+email = emails.findall(
+    texto)
+
+print()
+print("Los emails encontrados son: ")
+print()
+
+# bucle for para que nos de los correo en una linea diferente
+for e in email:
+    print(e)
+
+
+print(e)
+print()
+
+
+# TODO: Meter todo esto en una sola función
+# TODO: averiguar si es mejor una funcion que llame a las otras funciones o meterlo todo en una sola
